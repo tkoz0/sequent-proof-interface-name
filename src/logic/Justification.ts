faf362e2d66b7e3b2dedc319dc25fd4b90f9b94e
tkoz0 <null>
2022-04-22 18:36:48 -0400

    not elim implemented, skeleton for others

--- src/logic/Justification.ts
+++ src/logic/Justification.ts
@@ -1,6 +1,94 @@
+import {getSequent, setToList} from "../utils/LogicUtils";
+import ExprAny from "./ExprAny";
+import ExprNot from "./ExprNot";
 import {SequentCalc, SequentData} from "./Sequent";
 
 class Justification {
+    // Individual justification functions assume referenced sequents are valid.
+    // They modify calc.assumptions and set calc.valid
+    // calc.assumptions starts as an empty set
+    // Comment in the form of: Seq1,Seq2,... -> NewSeq
+    // Sequents are written as {expr1,expr2,...}:=Expr
+
+    // -> {P}:=P
+    public static justifyAssume(data: SequentData, calc: SequentCalc,
+            _seqData: SequentData[], _seqCalc: Map<string,SequentCalc>): void {
+        if (data.refs.size > 0)
+            calc.valid = false;
+        else {
+            calc.assumptions.add(data.id);
+            calc.valid = true;
+        }
+    }
+
+    // {G}:=~~P -> {G}:=P
+    private static notElimPattern = new ExprNot(new ExprNot(new ExprAny()));
+    public static justifyNotElim(data: SequentData, calc: SequentCalc,
+            seqData: SequentData[], seqCalc: Map<string,SequentCalc>): void {
+        if (data.expr === null || data.refs.size !== 1)
+            calc.valid = false;
+        else { // pattern match the ref expr
+            const refs = setToList(data.refs);
+            const [refData,refCalc] = getSequent(refs[0],seqData,seqCalc);
+            if (refData.expr === null)
+                throw "justifyNotElim: internal error";
+            if (Justification.notElimPattern.equals(refData.expr)) {
+                const refInnerExpr = refData.expr.values[0].values[0];
+                if (refInnerExpr.equals(data.expr)) {
+                    calc.valid = true;
+                    // copy assumptions from ref expr
+                    refCalc.assumptions.forEach(s => calc.assumptions.add(s));
+                    return;
+                }
+            }
+            calc.valid = false;
+        }
+    }
+
+    public static justifyNotIntro(data: SequentData, calc: SequentCalc,
+            seqData: SequentData[], seqCalc: Map<string,SequentCalc>): void {
+    }
+
+    public static justifyAndElim(data: SequentData, calc: SequentCalc,
+            seqData: SequentData[], seqCalc: Map<string,SequentCalc>): void {
+    }
+
+    public static justifyAndIntro(data: SequentData, calc: SequentCalc,
+            seqData: SequentData[], seqCalc: Map<string,SequentCalc>): void {
+    }
+
+    public static justifyOrElim(data: SequentData, calc: SequentCalc,
+            seqData: SequentData[], seqCalc: Map<string,SequentCalc>): void {
+    }
+
+    public static justifyOrIntro(data: SequentData, calc: SequentCalc,
+            seqData: SequentData[], seqCalc: Map<string,SequentCalc>): void {
+    }
+
+    public static justifyIfElim(data: SequentData, calc: SequentCalc,
+            seqData: SequentData[], seqCalc: Map<string,SequentCalc>): void {
+    }
+
+    public static justifyIfIntro(data: SequentData, calc: SequentCalc,
+            seqData: SequentData[], seqCalc: Map<string,SequentCalc>): void {
+    }
+
+    public static justifyIffElim(data: SequentData, calc: SequentCalc,
+            seqData: SequentData[], seqCalc: Map<string,SequentCalc>): void {
+    }
+
+    public static justifyIffIntro(data: SequentData, calc: SequentCalc,
+            seqData: SequentData[], seqCalc: Map<string,SequentCalc>): void {
+    }
+
+    public static justifyContElim(data: SequentData, calc: SequentCalc,
+            seqData: SequentData[], seqCalc: Map<string,SequentCalc>): void {
+    }
+
+    public static justifyContIntro(data: SequentData, calc: SequentCalc,
+            seqData: SequentData[], seqCalc: Map<string,SequentCalc>): void {
+    }
+
     /**
      * Checks a sequent for correctness and modifies the data arrays as
      * necessary.
@@ -15,52 +103,67 @@ class Justification {
         if (calc === undefined)
             throw "justify: nonexistent ID";
         const data = seqData[calc.index];
-        const refs: string[] = []; // list of the refs
-        data.refs.forEach(s => refs.push(s));
+        const refsData: SequentData[] = []; // list of the refs
+        const refsAssumes: Set<string>[] = [];
+        let anyInvalid = false; // check if any refs are not valid
+        data.refs.forEach(s => {
+            const calc2 = seqCalc.get(s);
+            if (calc2 === undefined)
+                throw "justify: nonexistent ID";
+            refsData.push(seqData[calc2.index]);
+            refsAssumes.push(calc2.assumptions);
+            if (!calc2.valid)
+                anyInvalid = true;
+        });
         const expr = data.expr; // logic expression
-        if (expr === null) { // null expression cannot be correct
-            calc.assumptions = new Set<string>();
+        calc.assumptions = new Set<string>(); // start with no assumptions
+        if (expr === null || anyInvalid) {
             calc.valid = false;
             return;
         }
-        const rule = data.rule; // justification rule
-        calc.assumptions = new Set<string>(); // start with no assumptions
-        switch (rule) {
+        // all refs are valid, expr is not null
+        switch (data.rule) {
             case "": // not justified
                 calc.valid = false;
                 break;
-            case "assume": // {psi} := psi
-                if (refs.length > 0) {
-                    calc.valid = false;
-                } else {
-                    calc.assumptions.add(id);
-                    calc.valid = true;
-                }
+            case "assume":
+                Justification.justifyAssume(data,calc,seqData,seqCalc);
                 break;
             case "notE":
-// TODO implement other justifications
+                Justification.justifyNotElim(data,calc,seqData,seqCalc);
                 break;
             case "notI":
+                Justification.justifyNotIntro(data,calc,seqData,seqCalc);
                 break;
             case "andE":
+                Justification.justifyAndElim(data,calc,seqData,seqCalc);
                 break;
             case "andI":
+                Justification.justifyAndIntro(data,calc,seqData,seqCalc);
                 break;
             case "orE":
+                Justification.justifyOrElim(data,calc,seqData,seqCalc);
                 break;
             case "orI":
+                Justification.justifyOrIntro(data,calc,seqData,seqCalc);
                 break;
             case "ifE":
+                Justification.justifyIfElim(data,calc,seqData,seqCalc);
                 break;
             case "ifI":
+                Justification.justifyIfIntro(data,calc,seqData,seqCalc);
                 break;
             case "iffE":
+                Justification.justifyIffElim(data,calc,seqData,seqCalc);
                 break;
             case "iffI":
+                Justification.justifyIffIntro(data,calc,seqData,seqCalc);
                 break;
             case "contE":
+                Justification.justifyContElim(data,calc,seqData,seqCalc);
                 break;
             case "contI":
+                Justification.justifyContIntro(data,calc,seqData,seqCalc);
                 break;
             default: // will never happen
                 throw "justify: invalid rule";
