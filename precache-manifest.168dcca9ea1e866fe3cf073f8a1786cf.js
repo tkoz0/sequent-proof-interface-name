28ae1acec969e03823bdffa872db7a54a35d896f
tkoz0 <null>
2022-04-25 01:56:54 -0400

    Updates

---
+++ precache-manifest.168dcca9ea1e866fe3cf073f8a1786cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2add28a8e928ce107e31",
+    "url": "/sequent-proof-interface-name/static/css/main.19f411c2.chunk.css"
+  },
+  {
+    "revision": "2add28a8e928ce107e31",
+    "url": "/sequent-proof-interface-name/static/js/main.fd47c226.chunk.js"
+  },
+  {
+    "revision": "f2139edb8f48c18b8178",
+    "url": "/sequent-proof-interface-name/static/js/runtime~main.2d4288b6.js"
+  },
+  {
+    "revision": "df97d55d6c7d4a8a5029",
+    "url": "/sequent-proof-interface-name/static/js/2.dd466389.chunk.js"
+  },
+  {
+    "revision": "469ae0393c9eca53ea73",
+    "url": "/sequent-proof-interface-name/static/js/3.268c5232.chunk.js"
+  },
+  {
+    "revision": "86194e890f2a52e91ed7c41d5ba3ba4b",
+    "url": "/sequent-proof-interface-name/index.html"
+  }
+];
