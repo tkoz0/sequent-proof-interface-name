48f1a6db8e1b7b93259ce010e67a9d71fac79b66
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2022-08-03 05:09:33 -0400

    comments

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,7 +1,8 @@
+// original name, changed to be something people can say comfortably
 //export const APP_TITLE_LONG = "Proof Interface for Sequent Systems";
 //export const APP_TITLE_SHORT = "PISS";
 export const APP_TITLE_LONG = "Sequent Proof Interface Name";
 export const APP_TITLE_SHORT = "SPIN";
 export const APP_TITLE_IS_TEMPORARY = false;
-export const ENABLE_PARSER_TEST = false;
+export const ENABLE_PARSER_TEST = false; // for debugging purposes
 export const FOOTER_MESSAGE = "Intermediate Logic Project by TKoz";
